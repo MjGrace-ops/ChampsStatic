c30b26df5c6c5a2cf90ac269f3bbbeb237d89b00
MjGrace-ops <mjzid81@gmail.com>
2026-06-04 11:38:13 +0000

    Incrementation des compteur

--- src/Personne.java
+++ src/Personne.java
@@ -1,4 +1,11 @@
 public class Personne {
 
+    // Attributs
+    public static int nbInstances;
+    public int nbLocal;
 
+    public Personne(){
+        nbInstances++;
+        nbLocal++;
+    }
 }
