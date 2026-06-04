a1714a6980bae2fc25a31eb37b047ac9de9032dd
MjGrace-ops <mjzid81@gmail.com>
2026-06-04 11:41:45 +0000

    Instanciation de la classe personne

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
+    public static void main(String[] args) {
+        // Instanciez 4 objets Personne
+        Personne personne1 = new Personne();
+        Personne personne2 = new Personne();
+        Personne personne3 = new Personne();
+        Personne personne4 = new Personne();
 
 
+        // Affichez les valeurs des deux compteurs
+        System.out.println("(" + personne1.nbLocal + "," +
+                Personne.nbInstances + ")");
+    }
 }
  
